2f39b11b263e4018c568ae9fc1f7ac6b086b845e
RayK <zrztt@outlook.com>
2020-12-11 09:25:51 +0800

    fix(bcc): 增加initBcc()，修复boardcast

--- bot/bcc.py
+++ bot/bcc.py
@@ -16,52 +16,53 @@ weather = TempWeatherData()
 config_admin = config['bot']['admin']
 
 
-@bcc.receiver("FriendMessage")
-async def friend_message_listener(app: GraiaMiraiApplication, friend: Friend,
-                                  message: MessageChain):
-    if friend.id == config_admin['master'] and message.asDisplay().startswith(
-            "/强制推送"):
-        groupDailyWeather()
-    elif message.asDisplay().startswith("/天气"):
-        await app.sendFriendMessage(
-            friend,
-            MessageChain.create(
-                [Plain(currentWeatherToStr(weather.getCurrentWeather()))]))
-    else:
-        await app.sendFriendMessage(
-            friend,
-            MessageChain.create([Plain("Hello, {}".format(friend.nickname))]))
+def initBcc():
+    @bcc.receiver("FriendMessage")
+    async def friend_message_listener(app: GraiaMiraiApplication,
+                                      friend: Friend, message: MessageChain):
+        if friend.id == config_admin['master'] and message.asDisplay(
+        ).startswith("/强制推送"):
+            groupDailyWeather()
+        elif message.asDisplay().startswith("/天气"):
+            await app.sendFriendMessage(
+                friend,
+                MessageChain.create(
+                    [Plain(currentWeatherToStr(weather.getCurrentWeather()))]))
+        else:
+            await app.sendFriendMessage(
+                friend,
+                MessageChain.create(
+                    [Plain("Hello, {}".format(friend.nickname))]))
 
+    inc = InterruptControl(bcc)
 
-inc = InterruptControl(bcc)
-
-
-@bcc.receiver("GroupMessage")
-async def group_message_handler(message: MessageChain,
-                                app: GraiaMiraiApplication, group: Group,
-                                member: Member):
-    if message.asDisplay().startswith("/当前天气"):
-        await app.sendGroupMessage(
-            group,
-            MessageChain.create([
-                At(member.id),
-                Plain('\n' + currentWeatherToStr(weather.getCurrentWeather()))
-            ]))
-    elif message.asDisplay().startswith("/今日天气"):
-        await app.sendGroupMessage(
-            group,
-            MessageChain.create([
-                At(member.id),
-                Plain('\n' + dailyWeatherToStr(weather.getDailyWeather()))
-            ]))
-    # if message.asDisplay().startswith("/刷新"):
-    #     await app.sendGroupMessage(group, MessageChain.create([
-    #         At(member.id), Plain("发送 /确认 确认强制刷新天气信息")
-    #     ]))
-    #     await inc.wait(GroupMessageInterrupt(
-    #         group, member,
-    #         custom_judgement=lambda x: x.messageChain.asDisplay().startswith("/确认")
-    #     ))
-    #     await app.sendGroupMessage(group, MessageChain.creat([
-    #         Plain("执行完毕.")
-    #     ]))
+    @bcc.receiver("GroupMessage")
+    async def group_message_handler(message: MessageChain,
+                                    app: GraiaMiraiApplication, group: Group,
+                                    member: Member):
+        if message.asDisplay().startswith("/当前天气"):
+            await app.sendGroupMessage(
+                group,
+                MessageChain.create([
+                    At(member.id),
+                    Plain('\n' +
+                          currentWeatherToStr(weather.getCurrentWeather()))
+                ]))
+        elif message.asDisplay().startswith("/今日天气"):
+            await app.sendGroupMessage(
+                group,
+                MessageChain.create([
+                    At(member.id),
+                    Plain('\n' + dailyWeatherToStr(weather.getDailyWeather()))
+                ]))
+        # if message.asDisplay().startswith("/刷新"):
+        #     await app.sendGroupMessage(group, MessageChain.create([
+        #         At(member.id), Plain("发送 /确认 确认强制刷新天气信息")
+        #     ]))
+        #     await inc.wait(GroupMessageInterrupt(
+        #         group, member,
+        #         custom_judgement=lambda x: x.messageChain.asDisplay().startswith("/确认")
+        #     ))
+        #     await app.sendGroupMessage(group, MessageChain.creat([
+        #         Plain("执行完毕.")
+        #     ]))
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from bot.bcc import initBcc
 from bot.master import app
 from scheduler.scheduler import startSchedule
 
+initBcc()
 startSchedule()
 app.launch_blocking()
